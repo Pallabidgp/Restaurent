81c72129b7cda9563e8bd144c936d2edae4c0f06
Pallabi Bayen <pallabibayen1022@gmail.com>
2025-02-26 15:04:04 +0530

    Adding Restaurent code

---
+++ assets/js/js.js
@@ -0,0 +1,74 @@
+	// banner 
+
+	// var swiper = new Swiper(".mySwiper", {
+	//           loop: true,
+	//           speed: 10000,
+	//             autoplay: {
+	//                 delay: 2000,
+	//               },
+	//           spaceBetween: 0,
+	//           pagination: {
+	//             el: ".swiper-pagination",
+	//             clickable: true,
+	//           },
+	//         });
+
+	//Service
+
+	// var swiper = new Swiper(".portfolio-slide", {
+  //           loop: true,
+  //           speed: 15000,
+  //           autoplay: {
+  //               delay: 5000,
+  //             },
+  //           pagination: {
+  //               el: ".swiper-pagination",
+  //               clickable: true,
+  //           },
+  //           navigation: {
+  //               nextEl: ".swiper-button-next",
+  //               prevEl: ".swiper-button-prev",
+  //           },
+  //           breakpoints: {
+  //               // when window width is >= 320px
+  //               100: {
+  //                 slidesPerView: 1.2,
+  //                 spaceBetween: 25
+  //               },
+  //               // when window width is >= 320px
+  //               768: {
+  //                 slidesPerView: 2.3,
+  //                 spaceBetween: 25
+  //               },
+  //               // when window width is >= 480px
+  //               1024: {
+  //                 slidesPerView: 4,
+  //                 spaceBetween: 25
+  //               },
+  //               // when window width is >= 640px
+  //               1280: {
+  //                 slidesPerView: 4,
+  //                 spaceBetween: 25
+  //               }
+  //           }
+  //       });
+
+	//Sidebar 
+
+		// function openNav() {
+    //       document.getElementById("detailsSidebar").style.width = "295px";
+    //     }
+
+    //     function closeNav() {
+    //       document.getElementById("detailsSidebar").style.width = "0";
+    //     }
+
+    // Sidemenu
+
+        // function sideMenu() {
+        //   document.getElementById("mySidenav").style.width = "250px";
+        // }
+
+        // function closeMenu() {
+        //   document.getElementById("mySidenav").style.width = "0";
+        // }
